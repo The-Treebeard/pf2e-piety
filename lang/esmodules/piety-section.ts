26a344cb6ebfd5820b71624b5ac4d251393954d5
Jacob Tibbitts <45647359+13jtibbitts@users.noreply.github.com>
2024-01-24 12:28:41 -0700

    Create piety-section.ts
    Added pseudocode for roadmap.
    Added empty class and hooks.

---
+++ lang/esmodules/piety-section.ts
@@ -0,0 +1,20 @@
+/* PSEUDOCODE:
+- Add Pf2ePiety class
+- Add method to add edicts and method to add anathemas. (Pf2e biography style)
+-- Add array of edicts and anathemas.
+- Add the ability to drag-and-drop Feat/Features with "Deity Boon"  to sections.
+- Get access to player's chosen deity and display it.
+
+- Reference my settings.ts to get the numbers for Piety threshhold.
+*/
+
+export class Pf2ePiety {
+  
+}
+
+Hooks.once('init', Pf2ePiety.init);
+Hooks.once('ready', Pf2ePiety.ready);
+
+// ADD: "dropover" event with dropTarget. See MonksAftermath.
+Hooks.on("renderCharacterSheetPf2e", async (charactersheet, html, data) => {
+}
