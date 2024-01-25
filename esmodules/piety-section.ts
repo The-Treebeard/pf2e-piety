8e00936f5003e62dccd99f2ff5ad0407fada778e
Jacob Tibbitts <45647359+13jtibbitts@users.noreply.github.com>
2024-01-25 12:32:36 -0700

    Update piety-section.ts
    Added a Hook to append Piety section to below the last list of the Effects Tab.

--- esmodules/piety-section.ts
+++ esmodules/piety-section.ts
@@ -9,7 +9,7 @@
 */
 
 export class Pf2ePiety {
-  
+  // Set new property on actor called pietyEdicts and pietyAnathema.
 }
 
 Hooks.once('init', Pf2ePiety.init);
@@ -17,8 +17,13 @@ Hooks.once('ready', Pf2ePiety.ready);
 
 // ADD: "dropover" event with dropTarget. See MonksAftermath.
 Hooks.on("renderCharacterSheetPf2e", async (charactersheet, html, data) => {
-  // Need const content.
+  const edicts = app.document.flags?.piety?.edicts ?? [];
+  // Append piety-section.hbs to below Divine Intercession list.
+  let pietyTemplate = await renderTemplate('modules/pf2e-piety/templates/piety-section.hbs'); // FIXME: Needs second argument of data.
+  let divineList = html.querySelectorAll('.effects .effects-list');
+  divineList[divineList.length-1].insertAdjacentHTML('afterend', pietyTemplate);
+  
   $("div[data-field='pietyEdicts'] a[data-action='add-piety-edict-anathema']", content).on("click", async (event) => {
-    // EDIT: Add code here.
+    await setFlag("pf2e-piety", "edicts", "edicts.push()");
   }
 }
