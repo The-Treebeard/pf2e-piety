7c781c723f2f9b276d4f401139af6335adc59732
Jacob Tibbitts <13jtibbitts@gmail.com>
2024-01-27 22:27:51 -0700

    Updated files.

--- scripts/piety-section.js
+++ scripts/piety-section.js
@@ -8,15 +8,41 @@
 - Reference my settings.ts to get the numbers for Piety threshhold.
 */
 
+import {registerSettings } from "./settings.ts";
+
+export let setting = key => {
+  return game.settings.get("pf2e-piety", key);
+};
+
 export class Pf2ePiety {
   // Set new property on actor called pietyEdicts and pietyAnathema.
+  static app = null;
+
+  static async init() {
+    registerSettings();
+
+    Pf2ePiety.SOCKET = "module.pf2e-piety";
+  }
+
+  static async onMessage(data) {
+    switch (data.action) {
+        case 'sendMessage': {
+            if (setting("notification-on-advance"))
+                ui.notifications.info(data.message);
+        }
+    }
 }
 
+  static ready() {
+    game.socket.on(Pf2ePiety.SOCKET, Pf2ePiety.onMessage);
+  }
+}
+console.log("Starting Hooks.");
 Hooks.once('init', Pf2ePiety.init);
 Hooks.once('ready', Pf2ePiety.ready);
 
 // ADD: "dropover" event with dropTarget. See MonksAftermath.
-Hooks.on("renderCharacterSheetPf2e", async (charactersheet, html, data) => {
+Hooks.on("renderPartySheetPF2e", async (charactersheet, html, data) => {
   const edicts = app.document.flags?.piety?.edicts ?? [];
   // Append piety-section.hbs to below Divine Intercession list.
   let pietyTemplate = await renderTemplate('modules/pf2e-piety/templates/piety-section.hbs'); // FIXME: Needs second argument of data.
@@ -25,5 +51,5 @@ Hooks.on("renderCharacterSheetPf2e", async (charactersheet, html, data) => {
   
   $("div[data-field='pietyEdicts'] a[data-action='add-piety-edict-anathema']", content).on("click", async (event) => {
     await setFlag("pf2e-piety", "edicts", "edicts.push()");
-  }
-}
+  });
+});
